9eaaf644f5bb862098b50536de8b57542d6d9154
mmira100 <mmira100@yahoo.com>
2026-02-15 11:35:17 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -8,18 +8,50 @@ from datetime import datetime
 
 app = FastAPI()
 
+#Veririfica que existan el folder para guardar la info recibida
 folder_name = "bpo_payloads"
-#1 comprobar si no existe la carpeta , crearla
+#Comprobar si no existe la carpeta , crearla
 if not os.path.exists(folder_name):
     os.makedirs(folder_name)
 
-with open("ejemplo1.txt", "w") as archivo:
-    archivo.write("Este es un archivo de texto creado con Python.")
+#Abrir el archivo de configuración para urls y credenciales
+from pathlib import Path
+ruta_base = Path(__file__).parent 
+archivo_ruta = ruta_base / "config.json"
+with open(archivo_ruta, 'r') as f:
+    data = json.load(f) # Lee y convierte a diccionario en un solo paso
+    url_token_tms  = data["items"][0]["url_token_tms"]
+    client_id      = data["items"][0]["client_id"]
+    client_secret  = data["items"][0]["client_secret"]
+    scope          = data["items"][0]["scope"]
+
+#Arma la url del TMS BY token
+url = url_token_tms
+
+payload = {
+    'grant_type': 'client_credentials',
+    'client_id': 'addf3efe-1124-443c-aa9d-cb57e8f841a4',
+    'client_secret': 'Jh~8Q~szegYVOjEiTIw1oK-pc2cjv3MjB_L6HaS~',
+     'scope': 'https://blueyonderus.onmicrosoft.com/69adb04d-658f-4b86-a659-67fe0f23bd1f/.default'
+}
+
+
+
+headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
 
 estatus = "Recibido"
 
 @app.post("/test/bpo", status_code=status.HTTP_202_ACCEPTED)
 async def get_json_raw(request: Request):
+    #Consumir la API externa TMS BY token usando requests    
+    response = requests.post(url, data=payload, headers=headers)
+    #Procesar y obtener el token de TMS BY
+    if response.status_code == 200:
+        token_data = response.json()
+        access_token = token_data.get('access_token')
+    
     # 1. Leer el stream de bytes
     raw_body = await request.body()
     # 2. Parsear manualmente
@@ -31,7 +63,8 @@ async def get_json_raw(request: Request):
         #with open("bpo_payloads/ejemplo12.json", "w", encoding="utf-8") as f:
         with open(archivo, "w", encoding="utf-8") as f:
              json.dump(data, f, indent=4, ensure_ascii=False)
-        return {"Solicitud aceptada e información recibida, muchas gracias BPO.": fecha_str}
+        #return {"Solicitud aceptada e información recibida, muchas gracias BPO.": fecha_str}
+        return {access_token }
     except Exception:
         return {"error": "Formato inválido"}, 400
      
