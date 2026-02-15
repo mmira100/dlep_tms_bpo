1aeb2ef11f18da00aba7aa723ebb84de6a3d48c7
mmira100 <mmira100@yahoo.com>
2026-02-15 11:54:27 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -63,8 +63,8 @@ async def get_json_raw(request: Request):
         #with open("bpo_payloads/ejemplo12.json", "w", encoding="utf-8") as f:
         with open(archivo, "w", encoding="utf-8") as f:
              json.dump(data, f, indent=4, ensure_ascii=False)
-        #return {"Solicitud aceptada e información recibida, muchas gracias BPO.": fecha_str}
-        return {access_token }
+        return {"Solicitud aceptada e información recibida, muchas gracias BPO.": fecha_str}
+        #return {access_token }
     except Exception:
         return {"error": "Formato inválido"}, 400
      
